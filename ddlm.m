828d6a1277c722ff109004f320ca5ea90d0f3632
Tuo Lin <tuo@tuos-mbp.attlocal.net>
2020-07-08 18:22:52 -0700

    ddlm

---
+++ ddlm.m
@@ -0,0 +1,56 @@
+function d = ddlm(z,rho,nnb,D)
+% ddlm(z,rho,nnb,D) computes the density of local maxima using the discrete method.
+%--------------------------------------------------------------------------
+% ARGUMENTS
+% z             a vector of local maxima value.
+% rho           spatial correlation, dimension 3, in each direction lattice. Input should be a n*d matrix with n voxels and d dimensions.
+% nnb           number of neighbor in each direction lattice, can be 0,1,2. Input should be a n*d matrix with n voxels and d dimensions. 
+% D             the dimension of the image.
+%--------------------------------------------------------------------------
+% OUTPUT
+% the density of the discrete local maxima at value z.
+%--------------------------------------------------------------------------
+% EXAMPLES
+% z = -2:0.01:2;
+% rho = ones(10,3)*0.8;
+% nnb = ones(10,3);
+% dendlm = ddlm(z,rho,nnb,1);
+%
+switch D
+    case 1
+        ld = 1;
+        PQd = asin(sqrt((1-rho(z==0,1)^2)/2))/pi;
+    case 2
+        ld = 2;
+        PQd = asin(sqrt((1-rho(z==0,1)^2)/2))/pi*asin(sqrt((1-rho(z==0,2)^2)/2))/pi;
+    case 3
+        ld = 3;
+        PQd = asin(sqrt((1-rho(z==0,1)^2)/2))/pi*asin(sqrt((1-rho(z==0,2)^2)/2))/pi*asin(sqrt((1-rho(z==0,3)^2)/2))/pi;
+end
+phi=exp(-z.^2./2)./sqrt(2*pi);
+nz=length(z);
+PQ=zeros(1,nz);
+for i=1:nz
+    Q=1;
+    zi=z(i);
+    for l=1:ld
+        alpha=asin(sqrt((1-rho(i,l)^2)/2));
+        da=alpha/1000;
+        h=sqrt((1-rho(i,l))/(1+rho(i,l)));
+        zplus=max(zi,0);
+        zall=0:da:alpha;
+        f=exp(-1/2*h^2*zi^2./(sin(zall)).^2);
+        %I=integral(@(y) 1./(-2.*log(y).*sqrt(-2.*log(y)-hz^2)), 0, y0);
+        if nnb(i,l)==1
+            Q=Q*(1-erfc(h*zplus/sqrt(2))/2);
+        end
+        if nnb(i,l)==2
+            Q=Q*(1-erfc(h*zplus/sqrt(2))+sum(f)*da/pi);
+            %Q=Q*(1-erfc(hzplus/sqrt(2))+I*hz/pi);
+        end
+    end
+    PQ(i)=Q;
+end
+PQ(z==0) = PQd;
+d=PQ.*phi;
+return
