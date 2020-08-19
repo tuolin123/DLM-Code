82635e81ee31600948dcca224f6b89a64106d4d9
Tuo Lin <tuo@tuos-mbp.attlocal.net>
2020-08-19 13:28:41 -0700

    twoDsim

--- Discrete_covariance/discrete_covariance.m
+++ Discrete_covariance/discrete_covariance.m
@@ -12,65 +12,77 @@ function discov = discrete_covariance(nu, D)
 
 switch D
     case 1
-        L = ceil(5*nu); % how is the support defined
+        L = max(2,ceil(5*nu)); % how is the support defined
         dL = 1;
-        [X] = -L:dL:L;              
-        h = gaussian_kernel(D, nu, X); 
-        h = h / sqrt(sum((h(:).^2)));
+        [X] = -L:dL:L;
+        if nu ~= 0
+            h = gaussian_kernel(D, nu, X); % using the gaussian kernel
+            h = h / sqrt(sum((h(:).^2)));
 
-        % Convolution shortcut (we only need it near the origin)
-        tic
-        rhot2 = convn(h, h, 'same');
-        toc
-        rho0 = rhot2(L+1);
-        rho1 = rhot2(L+2);
-        rho2 = rhot2(L+3);
-        discov = [rho0 rho1 rho2];
+            % Convolution shortcut (we only need it near the origin)
+            %tic
+            rhot2 = convn(h, h, 'same'); % convolution of the gaussian kernel
+            %toc
+            rho0 = rhot2(L+1); % L+1 is the center of [-L, L]
+            rho1 = rhot2(L+2); % get the rho of two adjacent voxels
+            rho2 = rhot2(L+3); % get the rho of two voxels with eucledian distance 2
+            discov = [rho0 rho1 rho2];
+        else
+            discov = [1 0 0]; % independent case
+        end
         
     case 2
-        L = ceil(5*nu); % how is the support defined
+        L = max(2,ceil(5*nu)); % how is the support defined
         dL = 1;
         [X, Y] = meshgrid( (-L:dL:L) , ...
-                           (-L:dL:L ));              
-        h = gaussian_kernel(D, nu, sqrt((X.^2 + Y.^2))); 
-        h = h / sqrt(sum((h(:).^2)));
+                           (-L:dL:L ));
+        if nu ~= 0
+            h = gaussian_kernel(D, nu, sqrt((X.^2 + Y.^2))); 
+            h = h / sqrt(sum((h(:).^2)));
 
-        % Convolution shortcut (we only need it near the origin)
-        tic
-        rhot2 = convn(h, h, 'same');
-        toc
-        rho0 = rhot2(L+1, L+1);
-        rho1 = rhot2(L+1, L+2);
-        rho2 = rhot2(L+2, L+2);
-        rho4 = rhot2(L+1, L+3);
-        rho5 = rhot2(L+2, L+3);
-        rho8 = rhot2(L+3, L+3);
-        discov = [rho0 rho1 rho2 rho4 rho5 rho8];
+            % Convolution shortcut (we only need it near the origin)
+            %tic
+            rhot2 = convn(h, h, 'same');
+            %toc
+            rho0 = rhot2(L+1, L+1);
+            rho1 = rhot2(L+1, L+2);
+            rho2 = rhot2(L+2, L+2);
+            rho4 = rhot2(L+1, L+3);
+            rho5 = rhot2(L+2, L+3);
+            rho8 = rhot2(L+3, L+3);
+            discov = [rho0 rho1 rho2 rho4 rho5 rho8];
+        else
+            discov = [1 0 0 0 0 0]; %indepdent case
+        end
         
     case 3
-        L = ceil(5*nu); % how is the support defined
+        L = max(2,ceil(5*nu)); % how is the support defined
         dL = 1;
         [X, Y, Z] = meshgrid( (-L:dL:L) , ...
                               (-L:dL:L ) , ...
-                              (-L:dL:L ));                
-        h = gaussian_kernel(D, nu, sqrt((X.^2 + Y.^2 + Z.^2))); 
-        h = h / sqrt(sum((h(:).^2)));
+                              (-L:dL:L ));  
+        if nu ~= 0
+            h = gaussian_kernel(D, nu, sqrt((X.^2 + Y.^2 + Z.^2))); 
+            h = h / sqrt(sum((h(:).^2)));
 
-        % Convolution shortcut (we only need it near the origin)
-        tic
-        rhot2 = convn(h, h, 'same');
-        toc
-        rho0 = rhot2(L+1, L+1, L+1);
-        rho1 = rhot2(L+1, L+2, L+1);
-        rho2 = rhot2(L+1, L+2, L+2);
-        rho3 = rhot2(L+2, L+2, L+2);
-        rho4 = rhot2(L+1, L+3, L+1);
-        rho5 = rhot2(L+1, L+2, L+3);
-        rho6 = rhot2(L+2, L+2, L+3);
-        rho8 = rhot2(L+1, L+3, L+3);
-        rho9 = rhot2(L+2, L+3, L+3);
-        rho12 = rhot2(L+3, L+3, L+3);
-        discov = [rho0 rho1 rho2 rho3 rho4 rho5 rho6 rho8 rho9 rho12];
+            % Convolution shortcut (we only need it near the origin)
+            %tic
+            rhot2 = convn(h, h, 'same');
+            %toc
+            rho0 = rhot2(L+1, L+1, L+1);
+            rho1 = rhot2(L+1, L+2, L+1);
+            rho2 = rhot2(L+1, L+2, L+2);
+            rho3 = rhot2(L+2, L+2, L+2);
+            rho4 = rhot2(L+1, L+3, L+1);
+            rho5 = rhot2(L+1, L+2, L+3);
+            rho6 = rhot2(L+2, L+2, L+3);
+            rho8 = rhot2(L+1, L+3, L+3);
+            rho9 = rhot2(L+2, L+3, L+3);
+            rho12 = rhot2(L+3, L+3, L+3);
+            discov = [rho0 rho1 rho2 rho3 rho4 rho5 rho6 rho8 rho9 rho12];
+        else
+            discov = [1 0 0 0 0 0 0 0 0 0];
+        end
 end
 
 
