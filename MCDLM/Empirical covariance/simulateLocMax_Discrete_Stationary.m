e18d5ceb10fb75b334897902cb6801caa02f4d37
tulin@ucsd.edu <43549569+tuolin123@users.noreply.github.com>
2022-10-31 22:21:17 -0700

    add stationary

--- MCDLM/Empirical covariance/simulateLocMax_Discrete_Stationary.m
+++ MCDLM/Empirical covariance/simulateLocMax_Discrete_Stationary.m
@@ -1,21 +1,70 @@
-function locmax = simulateLocMax_Discrete_Stationary(D, sigma2, niters, nondiag)
-% simulateLocMax_Discrete_Stationary(D, sigma2, niters, nondiag) simulate the local 
-% maxima in an stationary gaussian field through theoretical distribution of
-% multivariate gaussian distribution.
+function locmax = simulateLocMax_Discrete_Stationary(D, sigma2, niters, nondiag, nsubj)
+% simulateLocMax_Discrete_Stationary(D, sigma2, niters, nondiag) generates 
+% the local maxima in an stationary gaussian field through simulating the
+% theoretical distribution of multivariate gaussian distribution or 
+% t-distribution, using the covariance matrix either from the theoretical 
+% or empirical calculation.
+%
+% This is a more general function than simulateLocMax.m and 
+% simulateLocMax_Discrete.m in that it allows any types of covariance
+% function, but the whole covariance matrix is needed.
 %--------------------------------------------------------------------------
 % ARGUMENTS
 % D             the dimension of the stationary field, at least 2.
-% sigma2        empirical covariance matrix estimated from the stationary 
-%               field, returned from empiricalCov().
-% niters        iteration times to generate the local maxima.
-% nondiag       whether we do not count the diagonal as the neighbor
+% sigma2        the covariance matrix estimated from the stationary 
+%               field, returned from empiricalCov() if it's estimated 
+%               empirically.
+% niters        the number of iterations to generate the local maxima,
+%               note that the number of local maxima generated will be
+%               smaller than this number since we throw away those not
+%               qualified as local maxima
+% nondiag       a 0/1 value which denotes for whether we only consider the
+%               partial connectivity case, default is 0
+% nsubj         the degrees of freedom used in multivariate t-statistics, 
+%               if specified, the function will use t-distribution
 %--------------------------------------------------------------------------
 % OUTPUT
-% the local maxima from theoretical multivariate gaussian distribution.
+% locmax        a set of simulated local maxima
 %--------------------------------------------------------------------------
+% EXAMPLES
+% %apply the method using empirical covariance function
+% Dim = [50 50]; nsubj = 100; D = length(Dim); FWHM = 2;
+% noise = noisegen(Dim, nsubj, FWHM);
+% sigma2 = empiricalCov(D, noise);
+% 
+% % performs bad with small sample size, need to ensure covariance spd
+% [V,U] = eig(sigma2);
+% U = max(10e-10, U);
+% sigma2 = V*U*V';
+% 
+% niters = 10000;
+% nondiag = 1;
+% s = simulateLocMax_Discrete_Stationary(D, sigma2, niters, nondiag);
+% 
 %
+% %apply the method using covariance function by calculation
+% nu = [0.6 1.3];
+% D = 2;
+% sigma2 = discrete_covariance_ellipsoid(nu, D);
+% 
+% niters = 10000;
+% nondiag = 1;
+% s = simulateLocMax_Discrete_Stationary(D, sigma2, niters, nondiag);
+%--------------------------------------------------------------------------
+% AUTHOR: Tuo Lin
+%--------------------------------------------------------------------------
+
 if nargin == 3
+    I_tstat = 0; % whether we use t-statistics
     nondiag = 0;
+end
+
+if nargin == 4
+    I_tstat = 0;
+end
+
+if nargin == 5
+    I_tstat = 1;
 end
 
 locmax = zeros(1,niters);
@@ -31,23 +80,37 @@ switch D
         diagind = sort([midpt-3.^(0:(D-1)), midpt+3.^(0:(D-1)), midpt]);
 end
 
-% using multivariate gaussian distribution
-R = mvnrnd(zeros(1,dimMat), sigma2, niters);
-if nondiag == 0
-    for iter = 1:niters
-        if max(R(iter,:)) == R(iter,midpt) % R(iter,midpt) > 
-            locmax(k) = R(iter,midpt);
-            k = k + 1;
+if I_tstat == 0
+    % using multivariate gaussian distribution
+    R = mvnrnd(zeros(1,dimMat), sigma2, niters);
+    if nondiag == 0
+        for iter = 1:niters
+            if max(R(iter,:)) == R(iter,midpt) 
+                locmax(k) = R(iter,midpt);
+                k = k + 1;
+            end
+        end
+    elseif nondiag == 1
+        for iter = 1:niters
+            if max(R(iter,diagind)) == R(iter,midpt)
+                locmax(k) = R(iter,midpt);
+                k = k + 1;
+            end
         end
     end
-elseif nondiag == 1
+else
+    % using t distribution
+    R = (mvnrnd(zeros(1,dimMat), sigma2, niters*nsubj))';
+    R = reshape(R,[dimMat,niters,nsubj]);
+    Rtstat = mvtstat(R,[dimMat,niters]);
     for iter = 1:niters
-        if max(R(iter,diagind)) == R(iter,midpt)
-            locmax(k) = R(iter,midpt);
-            k = k + 1;
+        if Rtstat(midpt,iter) == max(Rtstat(:,iter))
+            locmax(k) = max(Rtstat(:,iter));
+            k = k+1;
         end
     end
 end
+
 
 
 locmax = locmax(1:(k-1));
--- MCDLM/main function/MCDLM.m
+++ MCDLM/main function/MCDLM.m
@@ -1,5 +1,5 @@
 function [peaklocs, peakinds, peakvals, peakpvals] = MCDLM(lat_data,...
-    mask, D, FWHM, gaussianize, sigma, niter, nlim, nj, nondiag)
+    mask, D, FWHM, isotropic, gaussianize, sigma, niter, nlim, nj, nondiag)
 % MCDLM(lat_data, mask, D, FWHM, sigma, niter, nlim, nj, nondiag) find the
 % top local maxima of an image that lies in a mask, and calculate the
 % p-values of these local maxima by using MCDLM approach with 
@@ -13,6 +13,8 @@ function [peaklocs, peakinds, peakvals, peakpvals] = MCDLM(lat_data,...
 %           apply and 0 for not, default is 0   
 % FWHM      the FWHM of smoothing kernel applied to smooth the original
 %           images
+% isotropic a 0/1 value denotes for whether the user want to use the
+%           isotropic property of the field, default is 1
 % sigma     the variance of the field, default is 1
 % niter     the iteration time used for simulateLocMax_Discrete()
 % nlim      the number of local maxima for the true height distribution
@@ -34,18 +36,31 @@ function [peaklocs, peakinds, peakvals, peakpvals] = MCDLM(lat_data,...
 %                   calculated for peakvals or gaussianized peakvals
 %--------------------------------------------------------------------------
 % EXAMPLES
-%2D example
+%2D example (isotropic)
 % Dim = [91,109];
 % D = length(Dim);
 % nimgs = 20;
 % FWHM = 1.4;
 % gaussianize = 0;
+% isotropic = 1;
 % lat_data = normrnd(0,1,[Dim,nimgs]);
 % mask = zeros(Dim); mask(30:60,40:80) = 1; mask = logical(mask);
 % [peaklocs, peakinds, peakvals, peakpvals] =...
-% MCDLM(lat_data, mask, D, FWHM, gaussian);
+% MCDLM(lat_data, mask, D, FWHM, isotropic, gaussianize);
 %
-% %3D example
+%2D example (non isotropic)
+% Dim = [91,109];
+% D = length(Dim);
+% nimgs = 20;
+% FWHM = 1.4;
+% gaussianize = 0;
+% isotropic = 0;
+% lat_data = normrnd(0,1,[Dim,nimgs]);
+% mask = zeros(Dim); mask(30:60,40:80) = 1; mask = logical(mask);
+% [peaklocs, peakinds, peakvals, peakpvals] =...
+% MCDLM(lat_data, mask, D, FWHM, isotropic, gaussianize);
+%
+% %3D example (with gaussianization)
 % Dim = [91,109,91];
 % D = length(Dim);
 % nimgs = 20;
@@ -54,12 +69,15 @@ function [peaklocs, peakinds, peakvals, peakpvals] = MCDLM(lat_data,...
 % lat_data = normrnd(0,1,[Dim,nimgs]);
 % mask = zeros(Dim); mask(30:60,40:80,30:60) = 1; mask = logical(mask);
 % [peaklocs, peakinds, peakvals, peakpvals] =...
-% MCDLM(lat_data, mask, D, FWHM, gaussian);
+% MCDLM(lat_data, mask, D, FWHM, isotropic, gaussianize);
 %--------------------------------------------------------------------------
 % AUTHOR: Tuo Lin
 %--------------------------------------------------------------------------
 if ~exist('FWHM', 'var')
     FWHM = 0;
+end
+if ~exist('isotropic', 'var')
+    isotropic = 1;
 end
 if ~exist('gaussian', 'var')
     gaussianize = 0;
@@ -110,40 +128,80 @@ end
 % standardize
 f_new = f_new./std(f_new, 0, D+1);
 
-% estimate the correlation
-[rho_est, sigma_est] = estsd(f_new, mask, D, 0.5); %if we assume stationarity
-rho = discrete_covariance(mean(sigma_est), D);
+% estimate the sd of smoothing kernel
+    [~, sigma_est] = estsd(f_new, mask, D, 0.5); 
 
-% run MCDLM with calculated rho
-if gaussianize == 0
-    locmaxZ1 = [];
+if isotropic == 1
+    % transfer into the spatial correlation
+    rho = discrete_covariance(mean(sigma_est), D);
     
-    for j = 1:nj
-        locmaxZ_sigma1 = simulateLocMax_Discrete(D,rho,sigma,niter,nondiag,nimgs); %apply MCDLM to t-stats
-        locmaxZ1 = [locmaxZ_sigma1 locmaxZ1];
-        if length(locmaxZ1) > nlim
-            break;
+    % run MCDLM with calculated rho
+    if gaussianize == 0
+        locmaxZ1 = [];
+        
+        for j = 1:nj
+            locmaxZ_sigma1 = simulateLocMax_Discrete(D,rho,sigma,niter,nondiag,nimgs); %apply MCDLM to t-stats
+            locmaxZ1 = [locmaxZ_sigma1 locmaxZ1];
+            if length(locmaxZ1) > nlim
+                break;
+            end
         end
+        
+        [empf, empz] = ecdf(locmaxZ1);
+        empz = empz(2:end); empf = empf(2:end);   %remove non-unique point
+        peakpvals = 1-interp1(empz,empf,peakvals);
+    else
+        % MCDLM method by applying the gaussianization
+        locmaxZ2 = [];
+        for j = 1:nj
+            locmaxZ_sigma2 = simulateLocMax_Discrete(D,rho,sigma,niter,nondiag); %apply MCDLM to Gaussian field
+            locmaxZ2 = [locmaxZ_sigma2 locmaxZ2];
+            if length(locmaxZ2) > nlim
+                break;
+            end
+        end
+        
+        peakvals_gauss = norminv(tcdf(peakvals, nimgs));
+        [empf2, empz2] = ecdf(locmaxZ2);
+        empz2 = empz2(2:end); empf2 = empf2(2:end);   %remove non-unique point
+        peakpvals = 1-interp1(empz2,empf2,peakvals_gauss);
     end
-    
-    [empf, empz] = ecdf(locmaxZ1);
-    empz = empz(2:end); empf = empf(2:end);   %remove non-unique point
-    peakpvals = 1-interp1(empz,empf,peakvals);
 else
-    % MCDLM method by applying the gaussianization
-    locmaxZ2 = [];
-    for j = 1:nj
-        locmaxZ_sigma2 = simulateLocMax_Discrete(D,rho,sigma,niter); %apply MCDLM to Gaussian field
-        locmaxZ2 = [locmaxZ_sigma2 locmaxZ2];
-        if length(locmaxZ2) > nlim
-            break;
+    % transfer into the correlation
+    rho = discrete_covariance_ellipsoid(sigma_est, D);
+    
+    % run MCDLM with calculated rho
+    if gaussianize == 0
+        locmaxZ1 = [];
+        
+        for j = 1:nj
+            locmaxZ_sigma1 = simulateLocMax_Discrete_Stationary(D,rho,niter,nondiag,nimgs); %apply MCDLM to t-stats
+            locmaxZ1 = [locmaxZ_sigma1 locmaxZ1];
+            if length(locmaxZ1) > nlim
+                break;
+            end
         end
+        
+        [empf, empz] = ecdf(locmaxZ1);
+        empz = empz(2:end); empf = empf(2:end);   %remove non-unique point
+        peakpvals = 1-interp1(empz,empf,peakvals);
+    else
+        % MCDLM method by applying the gaussianization
+        locmaxZ2 = [];
+        for j = 1:nj
+            locmaxZ_sigma2 = simulateLocMax_Discrete_Stationary(D,rho,niter,nondiag,nimgs); %apply MCDLM to Gaussian field
+            locmaxZ2 = [locmaxZ_sigma2 locmaxZ2];
+            if length(locmaxZ2) > nlim
+                break;
+            end
+        end
+        
+        peakvals_gauss = norminv(tcdf(peakvals, nimgs));
+        [empf2, empz2] = ecdf(locmaxZ2);
+        empz2 = empz2(2:end); empf2 = empf2(2:end);   %remove non-unique point
+        peakpvals = 1-interp1(empz2,empf2,peakvals_gauss);
     end
-    
-    peakvals_gauss = norminv(tcdf(peakvals, nimgs));
-    [empf2, empz2] = ecdf(locmaxZ2);
-    empz2 = empz2(2:end); empf2 = empf2(2:end);   %remove non-unique point
-    peakpvals = 1-interp1(empz2,empf2,peakvals_gauss);
-    
 end
 
+
+end
